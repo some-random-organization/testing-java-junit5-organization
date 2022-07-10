8cf76d24113b39358bb9866eb6a74295f30549be
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2022-07-10 22:35:21 +0200

    junit parameterized tests - csv file source

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -51,6 +52,13 @@ class OwnerTest implements ModelTests {
             "MI, 3, 3"
     })
     void csvInputTest(String stateName, int val1, int val2){
+        System.out.println(stateName + " " + val1 + " " + val2);
+    }
+
+    @DisplayName("csv from file test - ")
+    @ParameterizedTest(name = "{displayName} [{index}] {argumentsWithNames}")
+    @CsvFileSource(resources = "/input.csv", numLinesToSkip = 1)
+    void csvFromFileTest(String stateName, int val1, int val2){
         System.out.println(stateName + " " + val1 + " " + val2);
     }
 
